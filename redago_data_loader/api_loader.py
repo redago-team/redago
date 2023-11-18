d86cd4cf7e54f8588a85aa1d60a236e5fe5f6de6
ack2406 <ack9222@gmail.com>
2023-11-18 14:44:22 +0100

    feat(redago_data_loader): add project

---
+++ redago_data_loader/api_loader.py
@@ -0,0 +1,77 @@
+import requests
+import logging
+import random
+import os
+
+
+from data_preparer import DataPreparer
+
+logging.basicConfig(level=logging.INFO)
+
+class WolneLekturyAPILoader:
+    api_url = "https://wolnelektury.pl/api/"
+    data_preparer = DataPreparer()
+
+    def fetch_books(self, limit=10, genre="epika", shuffle=True, directory="books"):
+
+        books_url = f"{self.api_url}kinds/{genre}/books"
+
+        logging.info(f"Fetching books from {books_url}")
+        books = requests.get(books_url).json()
+
+        if shuffle:
+            random.shuffle(books)
+
+        for book in books:
+            
+            if limit <= 0:
+                break
+
+            book_details = requests.get(book['href']).json()
+            book_url = book_details['txt']
+            book_name = f"{book_details['txt'].split('/')[-1]}"
+
+            if not book_url:
+                logging.warning(f"Book {book_name} has no txt version")
+                continue
+
+            logging.info(f"Fetching book from {book_url}")
+            book_content = requests.get(book_url).text
+
+            if not book_content:
+                logging.warning(f"Book {book_name} has no content")
+                continue
+
+            logging.info(f"Processing book {book_name}")
+            processed_book = self.data_preparer.process_book(book_content)
+
+            if not processed_book:
+                logging.warning(f"Book {book_name} has no processed content")
+                continue
+
+            if os.path.exists(f"{directory}/{book_name}"):
+                logging.warning(f"Book {book_name} already exists")
+                continue
+
+            logging.info(f"Yielding book {book_name}")
+            yield {
+                'name': book_name,
+                'content': processed_book.encode('utf-8')
+            }
+
+            limit -= 1
+
+    def fetch_and_save_books(self, limit=10, genre="epika", shuffle=True, directory="books"):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        for book in self.fetch_books(limit=limit, genre=genre, shuffle=shuffle):
+            logging.info(f"Saving book {book['name']}")
+            with open(f"{directory}/{book['name']}", 'wb') as f:
+                f.write(book['content'])
+
+
+if __name__=="__main__":
+    loader = WolneLekturyAPILoader()
+
+    loader.fetch_and_save_books(limit=10, genre="epika", shuffle=True, directory="books")
---
+++ redago_data_loader/data_preparer.py
@@ -0,0 +1,39 @@
+import string
+
+class DataPreparer:
+    text_filter = string.ascii_letters + string.digits + ' .,?!' + 'ąęćłńóśźżĄĘĆŁŃÓŚŹŻ'
+    lines_to_remove = 5
+    words_per_line = 5
+
+    def process_book(self, raw_data: str):
+        # make one sentence per line with . ? ! as end of sentence
+        raw_data = raw_data.replace('. ', '.\n')
+        raw_data = raw_data.replace('! ', '!\n')
+        raw_data = raw_data.replace('? ', '?\n')
+
+        # remove sentences that have characters not belonging to self.text_filter
+        data = [line for line in raw_data.split('\n') if all(c in self.text_filter for c in line)]
+
+        # remove empty lines
+        data = [line for line in data if line]
+
+        # in every line, remove sentences that have less than self.words_per_line words
+        data = [line for line in data if len(
+            line.split(' ')) >= self.words_per_line]
+
+        # remove last self.lines_to_remove lines
+        data = data[:-self.lines_to_remove]
+
+        # make first letter of every sentence uppercase
+        try:
+            data = [line[0].upper() + line[1:] for line in data]
+        except IndexError:
+            pass
+
+        # remove lines that ends with words shorter than two characters (probably abbreviations)
+        data = [line for line in data if len(line.split(' ')[-1]) > 3]
+
+        # remove lines that ends with word which contains dot not as end of sentence
+        data = [line for line in data if '.' not in line.split(' ')[-1][:-1]]
+
+        return "\n".join(data)
