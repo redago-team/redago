e4fe78819fa4014808381180a05a9f6377d5e8d8
ack2406 <ack9222@gmail.com>
2023-12-02 19:47:59 +0100

    feat(redago_backend): add correct-tokenized path

--- redago_backend/app.py
+++ redago_backend/app.py
@@ -15,6 +15,9 @@ class TextCorrectionRequest:
 class TextResponse(BaseModel):
     text: str
 
+class TokenizedTextResponse(BaseModel):
+    tokenized_text: list
+
 
 corrector = Corrector([CommaCorrector(), SimpleCorrector()])
 
@@ -24,7 +27,47 @@ async def correct_text(data: TextCorrectionRequest) -> TextResponse:
     corrected_text = corrector.correct(data.text)
     return TextResponse(text=corrected_text)
 
+@post("/correct-tokenized")
+async def correct_text_tokenized(data: TextCorrectionRequest) -> TokenizedTextResponse:
+    corrected_text = corrector.correct(data.text)
+    text = data.text
+
+    tokenized_text = []
+
+    for text_word, corrected_word in zip(text.split(), corrected_text.split()):
+        reason = ["ok"]
+        if text_word != corrected_word:
+            reason.clear()
+            if text_word.lower() == corrected_word.lower():
+                reason.append("case")
+
+            if corrected_word[-1] == "," and text_word[-1] != ",":
+                reason.append("comma")
+                if corrected_word[:-1].lower() != text_word.lower():
+                    reason.append("ortography")
+            
+            if text_word[-1] == "," and corrected_word[-1] != ",":
+                reason.append("nocomma")
+                if corrected_word.lower() != text_word[:-1].lower():
+                    reason.append("ortography")
+
+            if corrected_word[-1] == "." and text_word[-1] != ".":
+                reason.append("period")
+                if corrected_word[:-1].lower() != text_word.lower():
+                    reason.append("ortography")
+
+            if corrected_word[-1] not in [".", ","] and text_word[-1] not in [".", ","] and corrected_word.lower() != text_word.lower():
+                reason.append("ortography")
+        
+        tokenized_text.append({
+            "text": text_word,
+            "corrected": corrected_word,
+            "reason": reason
+        })
+
+    return TokenizedTextResponse(tokenized_text=tokenized_text)
+
 
 cors_config = CORSConfig(allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])    
 
-app = Litestar([correct_text], cors_config=cors_config)
+app = Litestar([correct_text, correct_text_tokenized], cors_config=cors_config)
