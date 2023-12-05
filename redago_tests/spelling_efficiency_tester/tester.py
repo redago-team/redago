b7bd501acbd34631d829651cadb7596112b1fcb8
ack2406 <ack9222@gmail.com>
2023-12-05 22:04:15 +0100

    Refactor save_incorrect method to include error sentence

--- redago_tests/spelling_efficiency_tester/tester.py
+++ redago_tests/spelling_efficiency_tester/tester.py
@@ -25,17 +25,19 @@ class Tester:
 
         return sentence.replace(",", " ").strip().split()
 
-    def save_incorrect(self, sentence: str, corrected_sentence: str) -> None:
+    def save_incorrect(self, sentence: str, corrected_sentence: str, error_sentence) -> None:
         """
         Saves incorrect sentence to file.
         """
 
         with open("incorrect.txt", "a", encoding="utf-8") as f:
             f.write(f"ORIGINAL:\t{sentence}\n")
-            f.write(f"CORRECTED:\t{corrected_sentence}\n\n")
+            f.write(f"CORRECTED:\t{corrected_sentence}\n")
+            f.write(f"ERROR:\t{error_sentence}\n\n")
+
 
     def _rate_sentence(
-        self, corrected_sentence: str, original_sentence: str
+        self, corrected_sentence: str, original_sentence: str, error_sentence: str
     ) -> dict[str, int]:
         """
         Rates correctness of corrected sentence.
@@ -44,39 +46,53 @@ class Tester:
         # split sentences into words
         corrected_sentence = self.split_sentence(corrected_sentence)
         original_sentence = self.split_sentence(original_sentence)
+        error_sentence = self.split_sentence(error_sentence)
 
         # create dict for points
-        points: dir[str, int] = {"correct": 0, "incorrect": 0}
+        # true positive - word was errored and corrected
+        # false positive - word was errored but not corrected
+        # false negative - word was not errored but corrected
+        points: dir[str, int] = {"true positive": 0, "false positive": 0, "false negative": 0}
 
         # iterate through both documents
-        for word1, word2 in zip(corrected_sentence, original_sentence):
-            # if words are the same
-            if word1 == word2:
-                points["correct"] += 1
-            # if corrected word is different than original
+        for correct_word, original_word, error_word in zip(
+            corrected_sentence, original_sentence, error_sentence
+        ):
+            # if word was errored
+            if error_word != original_word:
+                # if word was corrected
+                if correct_word != original_word:
+                    points["true positive"] += 1
+                # if word was not corrected
+                else:
+                    points["false positive"] += 1
+            # if word was not errored
             else:
-                points["incorrect"] += 1
+                # if word was corrected
+                if correct_word != original_word:
+                    points["false negative"] += 1
         
 
         if SAVE_INCORRECT_PREDICTIONS and (
-            points["incorrect"] > 0
+            points["false positive"] > 0 or points["false negative"] > 0
         ):
-            self.save_incorrect(original_sentence, corrected_sentence)
+            self.save_incorrect(original_sentence, corrected_sentence, error_sentence)
 
         return points
 
-    def _rate(self, corrected_sentences, original_sentences) -> dict[str, int]:
+    def _rate(self, corrected_sentences, original_sentences, error_sentences) -> dict[str, int]:
         """
         Rates correctness of corrected sentences.
         """
-        points: dict[str, int] = {"correct": 0, "incorrect": 0}
+        points: dict[str, int] = {"true positive": 0, "false positive": 0, "false negative": 0}
 
-        for corrected_sentence, original_sentence in zip(
-            corrected_sentences, original_sentences
+        for corrected_sentence, original_sentence, error_sentence in zip(
+            corrected_sentences, original_sentences, error_sentences
         ):
-            result = self._rate_sentence(corrected_sentence, original_sentence)
-            points["correct"] += result["correct"]
-            points["incorrect"] += result["incorrect"]
+            result = self._rate_sentence(corrected_sentence, original_sentence, error_sentence)
+            points["true positive"] += result["true positive"]
+            points["false positive"] += result["false positive"]
+            points["false negative"] += result["false negative"]
 
         return points
 
@@ -98,8 +114,6 @@ class Tester:
         sentence = sentence.replace("j", "i")
 
         return sentence
-
-            
 
     def test(self, original_sentences: list[str] = []) -> dict[str, int]:
         # transform sentences to sentences with ortography errors
@@ -123,4 +137,4 @@ class Tester:
             print()
 
         # return rated result in format {'correct': int, 'incorrect': int, 'missing': int}
-        return self._rate(corrected_sentences, original_sentences)
+        return self._rate(corrected_sentences, original_sentences, error_sentences)
